ec48a407cfc0e6f6d85377e0451ba9a065589965
Matt Reid <matt.reid300@gmail.com>
2025-08-22 18:26:11 +0100

    Changed backend link

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://raw.githubusercontent.com/MattReid300/philanthropyjsontest/refs/heads/main/6_map_redeveloped.json',
+    baseURL: 'https://raw.githubusercontent.com/PhilanthropyIreland/json-map-data/refs/heads/main/pi-mapmarkers.json?token=GHSAT0AAAAAADJXGX755ZHB5SP2EBKKJSTY2FISZVA',
     //baseURL: 'https://gist.githubusercontent.com/intern17/f3b9d999a3d9920ac9d943340b3ff594/raw/7cee33775633961616eaf518db292baa16e8792f/PhilanthropyIrelandMap.json',
 });
 
