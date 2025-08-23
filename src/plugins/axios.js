2ad2b87e50d71977963b0f84e0765d4848f17e89
Matt Reid <matt.reid300@gmail.com>
2025-08-23 20:20:50 +0100

    Initial commit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://raw.githubusercontent.com/PhilanthropyIreland/json-map-data/refs/heads/main/pi-mapmarkers.json?token=GHSAT0AAAAAADJXGX755ZHB5SP2EBKKJSTY2FISZVA',
+    baseURL: 'https://raw.githubusercontent.com/PhilanthropyIreland/json-map-data/refs/heads/main/pi-mapmarkers-23082025.json?token=GHSAT0AAAAAADJXGX75UMNZ2QZYU4EMJRE22FKA4YA',
     //baseURL: 'https://gist.githubusercontent.com/intern17/f3b9d999a3d9920ac9d943340b3ff594/raw/7cee33775633961616eaf518db292baa16e8792f/PhilanthropyIrelandMap.json',
 });
 
