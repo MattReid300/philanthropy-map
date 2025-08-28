9cd954b7fdbacb801c497380bc6d01ce1e164ad3
Matt Reid <matt.reid300@gmail.com>
2025-08-29 00:46:29 +0200

    Private API

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'https://raw.githubusercontent.com/MattReid300/philanthropyjsontest/refs/heads/main/pi-mapmarkers.json',
-    baseURL: 'https://api.github.com/repos/PhilanthropyIreland/json-map-data/contents/pi-mapmarkers.json',
-    headers: {
-        Authorization: `Bearer ${process.env.VUE_APP_GITHUB_TOKEN}`,
-        Accept: "application/vnd.github.v3.raw" // ensures you get raw JSON
-    }
+    baseURL: 'https://raw.githubusercontent.com/MattReid300/philanthropyjsontest/refs/heads/main/pi-mapmarkers.json',
+    // baseURL: 'https://api.github.com/repos/PhilanthropyIreland/json-map-data/contents/pi-mapmarkers.json',
+    // headers: {
+    //     Authorization: `Bearer `,
+    //     Accept: "application/vnd.github.v3.raw" // ensures you get raw JSON
+    // }
 });
 
 export default api;
